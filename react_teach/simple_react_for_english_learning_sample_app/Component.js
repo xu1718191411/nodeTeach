135ec676538689c1f3578880f3c0015c88e560a1
XUZHONGWEI <sjo@curucuru.jp>
2019-10-10 15:02:01 +0900

    add a sample in js

---
+++ react_teach/simple_react_for_english_learning_sample_app/Component.js
@@ -0,0 +1,45 @@
+class BaseComponent extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <div>
+            <HeaderComponent/>
+            <ContentComponent/>
+        </div>
+    }
+}
+
+
+
+class HeaderComponent extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <div className={"menu"}>
+            <div>text</div>
+            <div>follow</div>
+            <div>sentence</div>
+            <div>vocabulary</div>
+            <div>my</div>
+        </div>
+    }
+}
+
+class ContentComponent extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <div className={"content"}>
+            <p>content</p>
+        </div>
+    }
+}
+
+
+
